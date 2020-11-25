f68a9783e99adeb74548b6b9639f97d0c1609f1e
pompy <nicole.lynn.pompei@gmail.com>
2020-11-25 12:44:38 -0500

    integer tests all pass, half of string tests pass and half throws a null pointer coming from the test utils sort?, will possibly revist to refactor

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -11,59 +11,124 @@ public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
         super(intArray);
     }
 
+
+
+
+
+
     @Override
     public Integer[] removeDuplicates(int maxNumberOfDuplications) {
-       Integer[] clonedArray = array.clone();
+        Integer[] arrayWithLessThanMaxDuplicates = new Integer[0];
+        Integer nextUniqueInt = 0;
 
-        for (int i = 0; i < clonedArray.length; i++) {
-            Integer currentVal = clonedArray[i];
-            int numberOfOccurrences = getNumberOfOccurences(currentVal);
-            boolean isToBeDeleted = numberOfOccurrences >= maxNumberOfDuplications;
-            
-            if(isToBeDeleted){
-               clonedArray = removeElementAtIndex(clonedArray, i);
-            }
-        }
-           return clonedArray; 
-        }
-        
-    
-        public Integer[] removeElementAtIndex(Integer[] arrayToBeCloned, int index){
-            Integer[] clonedArray = arrayToBeCloned.clone();
-            Integer[] leftArray =  Arrays.copyOfRange(clonedArray,0, index);
-            Integer[] rightArray = Arrays.copyOfRange(clonedArray, index + 1, arrayToBeCloned.length);
-            int newArrayLength = leftArray.length + rightArray.length;
-//            System.arraycopy();
-            Integer[] mergedArray = new Integer[newArrayLength];
-            int position = 0;
-
-            for(Integer element: leftArray){
-                mergedArray[position] = element;
-                position++;
-            }
-
-            for(Integer element: rightArray){
-                mergedArray[position] = element;
-                position++;
-            }
-
-            return mergedArray;
-        
-        }
-        private int getNumberOfOccurences(Integer currentVal){
-            int numOfOccurrences = 0;
-            for (int i = 0; i < array.length; i++) {
-                Integer arrayElement = array[i];
-                if(arrayElement.equals(currentVal)){
-                    numOfOccurrences++;
+        for (int i = 0; i < array.length; i += nextUniqueInt) {
+            Integer integerCounter = 1;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i].equals(array[j])) {
+                    integerCounter++;
+                    nextUniqueInt = integerCounter;
+                }
+                if (!(array[i].equals(array[j])) || j == array.length - 1) {
+                    nextUniqueInt = integerCounter; // next position to jump to in outer loop
+                    if (integerCounter < maxNumberOfDuplications) { //if counted ints are less than allowable, keep them
+                        for (int k = i; k < i + integerCounter; k++) {
+                            arrayWithLessThanMaxDuplicates = addToArray(arrayWithLessThanMaxDuplicates, array[k]);
+                        }
+                        break;
+                    }
+                    break;
                 }
             }
-            return numOfOccurrences;
         }
+        return arrayWithLessThanMaxDuplicates;
+    }
 
     @Override
     public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        Integer[] arrayWithoutExactNumOfDuplicates = new Integer[0];
+        Integer nextUniqueInteger = 0;
+        for (int i = 0; i < array.length; i += nextUniqueInteger) {
+            Integer integerCounter = 1;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i].equals(array[j])) {
+                    integerCounter++;
+                    nextUniqueInteger = integerCounter;
+                }
+                if (!(array[i].equals(array[j])) || j == array.length - 1) {
+                    nextUniqueInteger = integerCounter; //next position to jump to in outer loop
+                    if (integerCounter != exactNumberOfDuplications) { //counted elements less than elements to remove, keep elements
+                        for (int k = i; k < i + integerCounter; k++) {
+                            arrayWithoutExactNumOfDuplicates = addToArray(arrayWithoutExactNumOfDuplicates, array[k]);
+                        }
+                        break;
+                    }
+                    break;
+                }
 
-       return null;
+            }
+        }
+        return arrayWithoutExactNumOfDuplicates;
     }
+
+
+    public Integer[] addToArray(Integer[] arrayToAdd, Integer integerToAdd) {
+        Integer[] arrayToBeAddedTo = new Integer[arrayToAdd.length + 1];
+        System.arraycopy(arrayToAdd, 0, arrayToBeAddedTo, 0, arrayToAdd.length);
+        arrayToBeAddedTo[arrayToAdd.length] = integerToAdd;
+        return arrayToBeAddedTo;
+    }
+
+
+
 }
+
+
+
+
+//       Integer[] clonedArray = array.clone();
+//
+//        for (int i = 0; i < clonedArray.length; i++) {
+//            Integer currentVal = clonedArray[i];
+//            int numberOfOccurrences = getNumberOfOccurences(currentVal);
+//            boolean isToBeDeleted = numberOfOccurrences >= maxNumberOfDuplications;
+//
+//            if(isToBeDeleted){
+//               clonedArray = removeElementAtIndex(clonedArray, i);
+//            }
+//        }
+//           return clonedArray;
+//        }
+
+
+//        public Integer[] removeElementAtIndex(Integer[] arrayToBeCloned, int index){
+//            Integer[] clonedArray = arrayToBeCloned.clone();
+//            Integer[] leftArray =  Arrays.copyOfRange(clonedArray,0, index);
+//            Integer[] rightArray = Arrays.copyOfRange(clonedArray, index + 1, arrayToBeCloned.length);
+//            int newArrayLength = leftArray.length + rightArray.length;
+////            System.arraycopy();
+//            Integer[] mergedArray = new Integer[newArrayLength];
+//            int position = 0;
+//
+//            for(Integer element: leftArray){
+//                mergedArray[position] = element;
+//                position++;
+//            }
+//
+//            for(Integer element: rightArray){
+//                mergedArray[position] = element;
+//                position++;
+//            }
+//
+//            return mergedArray;
+//
+//        }
+//        private int getNumberOfOccurences(Integer currentVal){
+//            int numOfOccurrences = 0;
+//            for (int i = 0; i < array.length; i++) {
+//                Integer arrayElement = array[i];
+//                if(arrayElement.equals(currentVal)){
+//                    numOfOccurrences++;
+//                }
+//            }
+//            return numOfOccurrences;
+//        }
--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -1,5 +1,7 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/28/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
@@ -9,13 +11,71 @@ public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
         super(intArray);
     }
 
+
+
     @Override
     public String[] removeDuplicates(int maxNumberOfDuplications) {
-        return new String[0];
+        String [] arrayWithLessThanMaxDuplicates = new String[0];
+        Integer nextUniqueStringValue = 0;
+
+        for (int i = 0; i < array.length; i+=nextUniqueStringValue) {
+            Integer stringCounter = 1;
+            for (int j = i + 1; j < array.length ; j++) {
+                if(array[i].equals(array[j])){
+                    stringCounter++;
+                    nextUniqueStringValue = stringCounter;
+                }
+                if(!(array[i].equals(array[j])) || j == array.length -1){
+                    nextUniqueStringValue = stringCounter; //pushes i in outer loop to the next position
+                    if(stringCounter < maxNumberOfDuplications){
+                        for (int k = i; k < i + stringCounter; k++){
+                            arrayWithLessThanMaxDuplicates = addToArray(arrayWithLessThanMaxDuplicates, array[k]);
+                        }
+                        break;
+                    }
+                    break;
+                }
+
+            }
+
+        }
+
+        return arrayWithLessThanMaxDuplicates;
     }
 
     @Override
     public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
-        return new String[0];
+        String[] arrayWithLessThanExactNumOfDuplicates = new String[0];
+        Integer nextUniqueStringValue = 0;
+
+        for (int i = 0; i < array.length; i+=nextUniqueStringValue) {
+            Integer stringCounter = 1;
+
+            for (int j = i + 1; j < array.length; j++) {
+                if(array[i].equals(array[j])){
+                    stringCounter++;
+                    nextUniqueStringValue = stringCounter;
+                }
+                if(!(array[i].equals(array[j])) || j == array.length - 1){
+                    nextUniqueStringValue = stringCounter; //pushes the i outer loop to the next position
+                    if(stringCounter != exactNumberOfDuplications){
+                        for (int k = i; k < i + stringCounter ; k++) {
+                            arrayWithLessThanExactNumOfDuplicates = addToArray(arrayWithLessThanExactNumOfDuplicates, array[k]);
+                        }
+                        break;
+                    }
+                    break;
+                }
+            }
+        }
+        return arrayWithLessThanExactNumOfDuplicates;
+    }
+
+
+    public String[] addToArray(String[] arrayToAdd, String stringToAdd){
+        String[] arrayToBeAddedTo = new String[arrayToAdd.length + 1];
+        System.arraycopy(arrayToAdd, 0, arrayToAdd, 0, arrayToAdd.length);
+        arrayToBeAddedTo[arrayToAdd.length] = stringToAdd;
+        return arrayToBeAddedTo;
     }
 }
