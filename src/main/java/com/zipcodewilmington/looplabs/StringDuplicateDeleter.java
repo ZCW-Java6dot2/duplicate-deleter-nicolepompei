93a74460479806eb694813b6747f844bc705de81
pompy <nicole.lynn.pompei@gmail.com>
2020-11-27 09:38:13 -0500

    refactored string duplicate deleter

--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -12,70 +12,44 @@ public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
     }
 
 
-
     @Override
     public String[] removeDuplicates(int maxNumberOfDuplications) {
-        String [] arrayWithLessThanMaxDuplicates = new String[0];
-        Integer nextUniqueStringValue = 0;
+        String[] arrayWithLessThanMaxDuplicates = new String[0];
 
-        for (int i = 0; i < array.length; i+=nextUniqueStringValue) {
-            Integer stringCounter = 1;
-            for (int j = i + 1; j < array.length ; j++) {
-                if(array[i].equals(array[j])){
-                    stringCounter++;
-                    nextUniqueStringValue = stringCounter;
-                }
-                if(!(array[i].equals(array[j])) || j == array.length -1){
-                    nextUniqueStringValue = stringCounter; //pushes i in outer loop to the next position
-                    if(stringCounter < maxNumberOfDuplications){
-                        for (int k = i; k < i + stringCounter; k++){
-                            arrayWithLessThanMaxDuplicates = addToArray(arrayWithLessThanMaxDuplicates, array[k]);
-                        }
-                        break;
-                    }
-                    break;
-                }
-
+        for (int i = 0; i < this.array.length; i++) {
+            if (getNumberOfOccurences(this.array, this.array[i]) < maxNumberOfDuplications) {
+                int currentIndex = arrayWithLessThanMaxDuplicates.length;
+                arrayWithLessThanMaxDuplicates = Arrays.copyOf(arrayWithLessThanMaxDuplicates, currentIndex + 1);
+                arrayWithLessThanMaxDuplicates[currentIndex] = this.array[i];
             }
-
         }
-
         return arrayWithLessThanMaxDuplicates;
     }
+
 
     @Override
     public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
         String[] arrayWithLessThanExactNumOfDuplicates = new String[0];
-        Integer nextUniqueStringValue = 0;
-
-        for (int i = 0; i < array.length; i+=nextUniqueStringValue) {
-            Integer stringCounter = 1;
-
-            for (int j = i + 1; j < array.length; j++) {
-                if(array[i].equals(array[j])){
-                    stringCounter++;
-                    nextUniqueStringValue = stringCounter;
-                }
-                if(!(array[i].equals(array[j])) || j == array.length - 1){
-                    nextUniqueStringValue = stringCounter; //pushes the i outer loop to the next position
-                    if(stringCounter != exactNumberOfDuplications){
-                        for (int k = i; k < i + stringCounter ; k++) {
-                            arrayWithLessThanExactNumOfDuplicates = addToArray(arrayWithLessThanExactNumOfDuplicates, array[k]);
-                        }
-                        break;
-                    }
-                    break;
-                }
+        for (int i = 0; i < this.array.length; i++) {
+            if (getNumberOfOccurences(this.array, this.array[i]) != exactNumberOfDuplications) {
+                int currentIndex = arrayWithLessThanExactNumOfDuplicates.length;
+                arrayWithLessThanExactNumOfDuplicates = Arrays.copyOf(arrayWithLessThanExactNumOfDuplicates, currentIndex + 1);
+                arrayWithLessThanExactNumOfDuplicates[currentIndex] = this.array[i];
             }
         }
         return arrayWithLessThanExactNumOfDuplicates;
     }
 
 
-    public String[] addToArray(String[] arrayToAdd, String stringToAdd){
-        String[] arrayToBeAddedTo = new String[arrayToAdd.length + 1];
-        System.arraycopy(arrayToAdd, 0, arrayToAdd, 0, arrayToAdd.length);
-        arrayToBeAddedTo[arrayToAdd.length] = stringToAdd;
-        return arrayToBeAddedTo;
+    private int getNumberOfOccurences(String[] inputArray, String valueToCheck) {
+        int numberOfValueOccurrences = 0;
+
+        for (int i = 0; i < inputArray.length; i++) {
+            if (inputArray[i].equals(valueToCheck)) {
+                numberOfValueOccurrences++;
+            }
+        }
+        return numberOfValueOccurrences;
     }
+
 }
